a20b5f928efe7410ff11e27bbbb928dcde133d5f
shaukat39 <shayat39@yahoo.com>
2024-03-05 14:22:57 +0500

    areas calculation

---
+++ index.js
@@ -0,0 +1,28 @@
+/*Area of Rectangle*/
+
+let length=20
+let width=10
+
+var areaRectangle=length*width
+console.log("Area of Rectangle: "+areaRectangle );
+
+
+
+/*Area of Triangle*/
+
+let base=5
+let height=7
+
+var areaTriangle= 1/2*base*height
+console.log("Area of Triangle: "+areaTriangle);
+
+
+/*Area of Circle*/
+
+let pi=3.14
+let radius=4
+
+var areaCircle=pi*radius**2
+console.log("Area of Circle: "+areaCircle);
+
+
